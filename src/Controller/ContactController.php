b73e6d8bd02f8fcc69e44fa0ac7871f78d970409
Shuvi021 <yohan.lancri@efrei.net>
2026-01-08 15:48:02 +0100

    Ajout du formulaire dans contact et de l'envoie

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('contact/contact.html.twig');
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new TemplatedEmail())
+                ->from(new Address($data['email'], $data['name']))
+                ->to(new Address('admin@example.com', 'Site Admin'))
+                ->subject('Nouveau message de contact')
+                ->htmlTemplate('emails/contact_email.html.twig')
+                ->context(['data' => $data]);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé. Merci !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
     }
 }
